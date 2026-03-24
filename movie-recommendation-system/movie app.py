f316b49a1d0bd2756e18f27a369a555baff8b330
vsm2004 <128446657+vsm2004@users.noreply.github.com>
2026-03-24 06:53:01 +0530

    Add files via upload

---
+++ movie-recommendation-system/movie app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pickle
+
+# load data
+movies = pickle.load(open('movies.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+# recommendation function
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),
+                         reverse=True,
+                         key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+# UI
+st.title("🎬 Movie Recommendation System")
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie)
+    for movie in recommendations:
+        st.write(movie)
